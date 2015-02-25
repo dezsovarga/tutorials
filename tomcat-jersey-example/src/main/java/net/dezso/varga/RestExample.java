69e31890019db464b9adc43413b952109030d9a2
Dezso Varga <dezso.varga@softvisioninc.eu>
2015-02-25 17:42:37 +0200

    added tomcat jersey rest example

---
+++ tomcat-jersey-example/src/main/java/net/dezso/varga/RestExample.java
@@ -0,0 +1,19 @@
+package net.dezso.varga;
+
+import javax.ws.rs.GET;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.PathParam;
+
+@Path("hello")
+public class RestExample {
+
+    @GET
+    @Path("{name}")
+    public String sayHello(@PathParam("name") String name){
+        StringBuilder stringBuilder = new StringBuilder("SandBox | Hello ");
+        stringBuilder.append(name).append("!");
+
+        return stringBuilder.toString();
+    }
+
+}
